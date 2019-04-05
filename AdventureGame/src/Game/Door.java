17c09ec71c22bfd3dfed77d08c8838b9b8a47f2d
Sholto <Sholto@DeathStar>
2019-04-05 16:32:03 +1300

    dungeon edits

--- AdventureGame/src/Game/Door.java
+++ AdventureGame/src/Game/Door.java
@@ -9,5 +9,21 @@ public class Door extends Sprite{
 	public Door (int x, int y) {
 		super(x,y);
 	}
+
+	public int getSpawnY() {
+		return spawnY;
+	}
+
+	public void setSpawnY(int spawnY) {
+		this.spawnY = spawnY;
+	}
+
+	public int getSpawnX() {
+		return spawnX;
+	}
+
+	public void setSpawnX(int spawnX) {
+		this.spawnX = spawnX;
+	}
 	
 }
