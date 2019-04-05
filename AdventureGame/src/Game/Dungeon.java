1a572078598a1f2d84b40d08d954e5e2f6c5b4cd
Sholto <Sholto@DeathStar>
2019-04-05 14:01:18 +1300

    Added lists

--- AdventureGame/src/Game/Dungeon.java
+++ AdventureGame/src/Game/Dungeon.java
@@ -1,7 +1,10 @@
 package Game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dungeon {
 
-	
+	List<Room> rooms = new ArrayList<Room>();
 	
 }
--- AdventureGame/src/Game/Room.java
+++ AdventureGame/src/Game/Room.java
@@ -1,7 +1,12 @@
 package Game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
 	
 	private int roomNumber;
+	List<Entity> entityList= new ArrayList<Entity>();
+	List<Sprite> spriteList= new ArrayList<Sprite>();
 	
 }
