f14c92a5e1ed7db1fec33f0de0ca6df1a72f179b
Sholto <Sholto@DeathStar>
2019-04-15 23:14:11 +1200

    bugfixing the bugfixes

--- AdventureGame/src/Game/Inventory.java
+++ AdventureGame/src/Game/Inventory.java
@@ -14,7 +14,7 @@ public class Inventory {
 	private List<Item> droppedItems = new ArrayList<Item>();
 	private List<BufferedImage> images = new ArrayList<BufferedImage>();
 	private Item placeholder = new Item(0,0);
-	
+
 	public Inventory(){
 		File f;
 		for (int i = 0; i < 8; i++) {
@@ -31,23 +31,23 @@ public class Inventory {
 			}
 		}
 	}
-	
+
 	public BufferedImage getImage() {
 		return this.images.get(index);
 	}
-	
+
 	public int inventorySize() {
 		return items.size();
 	}
-	
+
 	public Item seeItem(int i) {
 		return this.items.get(i);
 	}
-	
+
 	public Item getItem() {
 		return this.items.get(this.index);
 	}
-	
+
 	public boolean addItem(Item item) {
 		for (int i = 0; i < items.size(); i++)
 			if (items.get(i) == placeholder) {
@@ -59,48 +59,49 @@ public class Inventory {
 		else
 			return false;
 		return true;
-		
+
 	}
-	
+
 	public void dropItem() {
-		if (items.get(index) == placeholder)
-			return;
-		if (index == items.size()-1) {
-			droppedItems.add(getItem());
-			this.items.remove(this.index);
+		if (items.get(index) != placeholder) {
+			if (index == items.size()-1) {
+				droppedItems.add(getItem());
+				this.items.remove(this.index);
+			}
+			else if (index < items.size()-1) {
+				droppedItems.add(getItem());
+				this.items.set(index, placeholder);
+			}
 		}
-		else if (index < items.size()-1) {
-			droppedItems.add(getItem());
-			this.items.set(index, placeholder);
-		}
+
 	}
-	
+
 	public void incrementIndex() {
 		if (index == 7)
 			this.index = 0;
 		else
 			this.index++;
 	}
-	
+
 	public void decrementIndex() {
 		if (index == 0)
 			this.index = 7;
 		else
 			this.index--;
 	}
-	
+
 	public void setIndex(int index) {
 		this.index = index;
 	}
-	
+
 	public int droppedItemsSize() {
 		return droppedItems.size();
 	}
-	
+
 	public List<Item> getDroppedItems() {
 		return droppedItems;
 	}
-	
+
 	public void clearDroppedItems() {
 		droppedItems.clear();
 	}
